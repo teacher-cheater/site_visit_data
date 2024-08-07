b8a8a5b735e7f90b19dc7bc79af0622d53010c85
teacher-cheater <johnnykayfun@gmail.com>
2024-08-07 13:04:50 +0300

    data output

--- visitors/src/components/SessionData.vue
+++ visitors/src/components/SessionData.vue
@@ -5,17 +5,19 @@
       <thead>
         <tr>
           <th>ID</th>
-          <th>URL</th>
-          <th>Visiting Site</th>
-          <th>Time Stamp</th>
+          <th>Ссылка на сайт</th>
+          <th>Время нахождения</th>
+          <th>Время, когда заходили</th>
+          <th>USER_AGENT</th>
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <td>{{}}</td>
-          <td>{{}}</td>
-          <td>{{}}</td>
-          <td>{{}}</td>
+        <tr v-for="item in visits" :key="item.id">
+          <td>{{ item.id }}</td>
+          <td>{{ item.url }}</td>
+          <td>{{ item.time_on_page }}</td>
+          <td>{{ formatDate(item.time_stamp) }}</td>
+          <td>{{ item.user_agent }}</td>
         </tr>
       </tbody>
     </table>
@@ -39,6 +41,15 @@ export default {
     }
   },
   methods: {
+    formatDate(timestamp) {
+      const date = new Date(timestamp)
+      const day = String(date.getDate()).padStart(2, '0')
+      const month = String(date.getMonth() + 1).padStart(2, '0')
+      const year = date.getFullYear()
+      const hours = String(date.getHours()).padStart(2, '0')
+      const minutes = String(date.getMinutes()).padStart(2, '0')
+      return `${day}.${month}.${year} ${hours}:${minutes}`
+    },
     fetchVisits() {
       fetch('http://site-visit-data/collect.php')
         .then((response) => {
@@ -48,7 +59,7 @@ export default {
           return response.json()
         })
         .then((data) => {
-          console.log('Success:', data)
+          this.visits = data.data
         })
         .catch((error) => {
           console.error('Error:', error)
