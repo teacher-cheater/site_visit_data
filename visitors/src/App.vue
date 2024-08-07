098a1cab1fdecd7e38de77097b6d3da0e1fd0d4e
teacher-cheater <johnnykayfun@gmail.com>
2024-08-07 11:40:05 +0300

    add vue

---
+++ visitors/src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import SessionData from './components/SessionData.vue'
+</script>
+
+<template>
+  <header>
+    <h1>Тестовое задание для программиста ИТ(JS, VueJS, PHP, MySQL)</h1>
+  </header>
+
+  <main>
+    <SessionData />
+  </main>
+</template>
+
+<style></style>
---
+++ visitors/src/components/SessionData.vue
@@ -0,0 +1,11 @@
+<template>
+    123
+</template>
+
+<script setup>
+    console.log(444)
+</script>
+
+<style>
+    
+</style>
